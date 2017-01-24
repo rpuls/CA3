d0b3b91ca94c9a38043185975036fbe4e924ef64
rasmus <rasmus@DESKTOP-VP3V2CP>
2017-01-24 13:39:02 +0100

    openinghours now works for the current weekday. have changed so that dayNull i sunday to be consistent with javascripts way of getting weekdays

--- src/main/webapp/app/components/directives.js
+++ src/main/webapp/app/components/directives.js
@@ -45,7 +45,7 @@ angular.module('myApp.directives', [])
                     var modelSetter = model.assign;
 
                     element.bind('change', function () {
-                        if(this.files.length>4){
+                        if (this.files.length > 4) {
                             element[0].files = null;
                             alert('TOO MANY FILES. Please select 4 images only.');
                             scope = "";
@@ -58,5 +58,28 @@ angular.module('myApp.directives', [])
                 }
             };
         })
+
+        .directive('openingHours', function () {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    shop: '='
+                },
+                template: function () {
+                    var date = new Date();
+                    var weekDay = date.getDay();
+                    switch(weekDay){
+                        case 0 : return '<div>{{shop.dayNullOpen | hourFilter}} - {{shop.dayNullClose | hourFilter}}</div>';
+                        case 1 : return '<div>{{shop.dayOneOpen | hourFilter}} - {{shop.dayOneClose | hourFilter}}</div>';
+                        case 2 : return '<div>{{shop.dayTwoOpen | hourFilter}} - {{shop.dayTwoClose | hourFilter}}</div>';
+                        case 3 : return '<div>{{shop.dayThreeOpen | hourFilter}} - {{shop.dayThreeClose | hourFilter}}</div>';
+                        case 4 : return '<div>{{shop.dayFourOpen | hourFilter}} - {{shop.dayFourClose | hourFilter}}</div>';
+                        case 5 : return '<div>{{shop.dayFiveOpen | hourFilter}} - {{shop.dayFiveClose | hourFilter}}</div>';
+                        case 6 : return '<div>{{shop.daySixOpen | hourFilter}} - {{shop.daySixClose | hourFilter}}</div>';
+                    }
+                }
+            };
+        });
 ;
 
